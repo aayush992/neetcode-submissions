8f8f95c0d2cb24e02f033dae0d52e15ecde07635
Aayush Saini <132824984+aayush992@users.noreply.github.com>
2026-08-07 18:02:38 +0530

    Add: majority-element - submission-7

---
+++ Data Structures & Algorithms/majority-element/submission-7.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int a=0;
+        if(nums.size()<2)return nums[0];
+        for(int i=0;i<nums.size()-1;i++){
+            int c=1;
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j])
+                c++;
+            }
+              if(c>nums.size()/2)
+              a=nums[i];
+              
+        }
+        return a;
+        /*int count = 0;
+        int candidate = 0;
+        for(int i=0;i<nums.size();i++){
+            if( count == 0){
+                candidate = nums[i];
+            }
+            if(nums[i] == candidate )
+            count++;
+
+            else count--;
+        }
+        return candidate;*/
+    }
+};

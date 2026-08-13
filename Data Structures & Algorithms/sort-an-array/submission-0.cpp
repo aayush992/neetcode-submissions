6733cfcc35f8048eaddc83098e8b32118697bf70
Aayush Saini <132824984+aayush992@users.noreply.github.com>
2026-08-13 19:45:02 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:     
+    void merge(vector<int>& nums, int l, int m, int r){
+        int i = 0 , j = 0 , k = l;
+        int n1 = m-l+1;
+        int n2 = r-m;
+        vector<int>L(n1), R(n2);
+        for(int i=0; i<n1; i++){
+            L[i] = nums[l+i];
+        }
+        for(int j=0; j<n2; j++){
+            R[j] = nums[m+1+j];
+        }
+        while(i<n1 && j<n2){
+            if(L[i]<R[j]){
+                nums[k]=L[i];
+                i++;
+            }
+            else{
+                nums[k]=R[j];
+                j++;
+            }
+            k++;
+        }
+           // Remaining elements of L
+        while(i < n1) {
+            nums[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Remaining elements of R
+        while(j < n2) {
+            nums[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeSort(vector<int>& nums , int l, int r){
+        if(l < r){
+            int m = l + (r - l) / 2;
+            mergeSort(nums, l, m);
+            mergeSort(nums, m+1, r);
+            merge(nums, l, m, r);
+        }
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        int n= nums.size();
+        int l = 0;
+        int r = n-1;
+        mergeSort(nums, l, r);
+        return nums;
+    }
+};
